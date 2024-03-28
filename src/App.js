022b513a876971d581b92bdd0e9fbaf7654c2ce8
knllydgmntz <knllydgmntz@gmail.com>
2024-03-28 16:43:01 +0800

    Finalize expense tracking

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React from "react"; // need to import react from react
+import React, { useState } from "react"; // need to import react from react
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2024, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2024, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2024, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2024, 5, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2025, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2025, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2025, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2025, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
-
-  // Alternative in jsx code
-
-  // return React.createElement(
-  //   "div",
-  //   {},
-  //   React.createElement("h2", {}, "Let's get starded!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
 
   return (
     <div>
